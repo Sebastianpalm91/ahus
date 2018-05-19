50550868a77a55dcd3f176116c1b73b28206cba5
Sebastian Palm <sebastian.palm91@gmail.com>
2018-05-19 14:05:49 +0200

    made connection to Users/Contact table

--- ahus-pwa/src/Components/Views/Contact/Contact.js
+++ ahus-pwa/src/Components/Views/Contact/Contact.js
@@ -1,78 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
 import { Container, ItemContainer, Items, ContactInfo, ContactPicture, ContactText, Picture } from './styles';
 
-const Settings = () => (
-  <Container>
-      <ItemContainer>
-          <Items>
-              <ContactInfo>
-                  <ContactText>Jej</ContactText>
-              </ContactInfo>
-              <ContactPicture>
-                  <ContactText>Jej</ContactText>
-              </ContactPicture>
-          </Items>
-          <Items>
-              <ContactInfo>
-                  <ContactText>Jej</ContactText>
-              </ContactInfo>
-              <ContactPicture>
-                  <ContactText>Jej</ContactText>
-              </ContactPicture>
-          </Items>
-          <Items>
-              <ContactInfo>
-                  <ContactText>Jej</ContactText>
-              </ContactInfo>
-              <ContactPicture>
-                  <ContactText>Jej</ContactText>
-              </ContactPicture>
-          </Items>
-          <Items>
-              <ContactInfo>
-                  <ContactText>Jej</ContactText>
-              </ContactInfo>
-              <ContactPicture>
-                  <ContactText>Jej</ContactText>
-              </ContactPicture>
-          </Items>
-      </ItemContainer>
-      <ItemContainer>
-          <Items>
-              <ContactInfo>
-                  <ContactText>Jej</ContactText>
-              </ContactInfo>
-              <ContactPicture>
-                  <ContactText>Jej</ContactText>
-              </ContactPicture>
-          </Items>
-          <Items>
-              <ContactInfo>
-                  <ContactText>Jej</ContactText>
-              </ContactInfo>
-              <ContactPicture>
-                  <ContactText>Jej</ContactText>
-              </ContactPicture>
-          </Items>
-          <Items>
-              <ContactInfo>
-                  <ContactText>Jej</ContactText>
-              </ContactInfo>
-              <ContactPicture>
-                  <ContactText>Jej</ContactText>
-              </ContactPicture>
-          </Items>
-          <Items>
-              <ContactInfo>
-                  <ContactText>Jej</ContactText>
-              </ContactInfo>
-              <ContactPicture>
-                  <ContactText>Jej</ContactText>
-              </ContactPicture>
-          </Items>
-      </ItemContainer>
-  </Container>
-)
 
+class ContactList extends Component {
+  state = {
+    users: []
+  }
 
-export default Settings;
+  componentDidMount() {
+    axios.get(`http://127.0.0.1:8000/user`)
+      .then(res => {
+        const users = res.data.users;
+        this.setState({ users });
+        console.log(users);
+      })
+  }
+
+  render() {
+    return (
+        <Container>
+            {this.state.users.map(user =>
+                <ItemContainer key={user.id}>
+                    <Items>
+                        <ContactInfo>
+                            <ContactText>{ user.name }</ContactText>
+                            <ContactText>{ user.role }</ContactText>
+                            <ContactText>{ user.email }</ContactText>
+                            <ContactText>{ user.phone }</ContactText>
+                            <ContactPicture>
+                                { user.avatar }
+                            </ContactPicture>
+                        </ContactInfo>
+                    </Items>
+                </ItemContainer>
+            )}
+        </Container>
+    )
+  }
+}
+
+export default ContactList
