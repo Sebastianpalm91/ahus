09e3feca00ca397f3ac23c341fe6d6238c468946
Sebastian Palm <sebastian.palm91@gmail.com>
2018-05-23 23:18:57 +0200

    added content and frontend to News

--- ahus-pwa/src/Components/Views/News/News.js
+++ ahus-pwa/src/Components/Views/News/News.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import { Container, Wrapper, Items, ItemFooter, ItemDate } from './styles';
+import { Container, Wrapper, Items, ItemFooter, ItemDate, NewsTitle, NewsAuthor, TextWrapper, DateText, ContextImage } from './styles';
 
 const About = () => (
   <Container>
       <Wrapper>
           <Items>
-              <ItemDate></ItemDate>
-              <ItemFooter></ItemFooter>
+              <ItemDate>
+                    <DateText>
+                        30/12-2018
+                    </DateText>
+              </ItemDate>
+              <ItemFooter>
+                  <TextWrapper>
+                    <NewsTitle>
+                        Hej
+                    </NewsTitle>
+                    <NewsAuthor>
+                        / Kalle Kalas
+                    </NewsAuthor>
+                </TextWrapper>
+                <ContextImage>
+                </ContextImage>
+              </ItemFooter>
           </Items>
           <Items>
               <ItemDate></ItemDate>
