350e0b82c2f0017f1d5a4e07457f020ca0b87363
Johan Sylvan <gtc124@gmail.com>
2018-05-25 13:23:33 +0200

    Added eloquent relation between campus and location

--- app/Location.php
+++ app/Location.php
@@ -4,7 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\Campus;
+
 class Location extends Model
 {
     protected $fillable = ['name'];
+
+    function campus() {
+        return $this->belongsTo('App\Campus');
+    }
 }
